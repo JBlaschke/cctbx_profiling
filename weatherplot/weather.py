6fb97193aa0dd2caffe786118e2521e02b9ab82f
Johannes Blaschke <jpblaschke@lbl.gov>
2020-04-17 08:47:47 -0700

    start formalizing the weatherplot

--- weatherplot/weather.py
+++ weatherplot/weather.py
@@ -119,29 +119,6 @@ def combine_totals(data_dict):
 
 
 
-def compute_deltas(data_dict):
-
-    good_deltas   = list()
-    failed_deltas = list()
-
-    for file_name in data_dict:
-
-        data = data_dict[file_name]
-
-        good_t   = data["good_x"]
-        failed_t = data["failed_x"]
-
-        good_deltas += [
-            good_t[i+1] - good_t[i] for i in range(len(good_t) - 1)
-        ]
-        failed_deltas += [
-            failed_t[i+1] - failed_t[i] for i in range(len(failed_t) - 1)
-        ]
-
-    return good_deltas, failed_deltas
-
-
-
 def scan_reference(root):
 
     # make min work with "None"
