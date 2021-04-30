9d8375e17cecd0d8d664f4839f7b89a465ece183
Johannes Blaschke <jpblaschke@lbl.gov>
2021-04-30 10:51:20 -0700

    link to eq rather than copying

--- analysis/eq.py
+++ analysis/eq.py
@@ -4,14 +4,14 @@
 
 class EventQueue(object):
 
-    def to_delta(self, t_in):
+    def to_delta(self, t1, t2):
 
-        if self.end < t_in[0]:
+        if self.end < self._eq[t1]:
             return 0.
-        elif self.end > t_in[1]:
-            return t_in[1] - t_in[0]
+        elif self.end > self._eq[t2]:
+            return self._eq[t2] - self._eq[t1]
         else:
-            return self.end - t_in[0]
+            return self.end - self._eq[t1]
 
 
     def __init__(self, eq, good, rank):
@@ -26,27 +26,40 @@ class EventQueue(object):
 
         self._good = good
         self._rank = rank
+        self._eq   = eq
 
         # Events can have any number of steps
         self._end_index = 1
-        self._spin_up = (eq[0], eq[1])
+        self._spin_up = (0, 1)
         if len(eq) > 2:
-            self._spotfind = (eq[1], eq[2])
+            self._spotfind = (1, 2)
             self._post     = self._spotfind
             self._end_index = 2
         if len(eq) > 3:
-            self._index = (eq[2], eq[3])
+            self._index = (2, 3)
             self._post  = self._index
             self._end_index = 3
         if len(eq) > 4:
-            self._refine = (eq[3], eq[4])
+            self._refine = (3, 4)
             self._post   = self._refine
             self._end_index = 4
         if len(eq) > 5:
-            self._integrate = (eq[4], eq[5])
+            self._integrate = (4, 5)
             self._post      = self._integrate
             self._end_index = 5
-        self._end = eq[self._end_index]
+
+        self._start = eq[0]
+        self._end   = eq[self._end_index]
+
+
+    @property
+    def event_queue(self):
+        return self._eq
+
+
+    @property
+    def start(self):
+        return self._start
 
 
     @property
@@ -66,32 +79,32 @@ class EventQueue(object):
 
     @property
     def spin_up(self):
-        return self.to_delta(self._spin_up)
+        return self.to_delta(*self._spin_up)
 
 
     @property
     def spotfind(self):
-        return self.to_delta(self._spotfind)
+        return self.to_delta(*self._spotfind)
 
 
     @property
     def index(self):
-        return self.to_delta(self._index)
+        return self.to_delta(*self._index)
 
 
     @property
     def refine(self):
-        return self.to_delta(self._refine)
+        return self.to_delta(*self._refine)
 
 
     @property
     def integrate(self):
-        return self.to_delta(self._integrate)
+        return self.to_delta(*self._integrate)
 
 
     @property
     def post(self):
-        return self.to_delta(self._post)
+        return self.to_delta(*self._post)
 
 
     @property
