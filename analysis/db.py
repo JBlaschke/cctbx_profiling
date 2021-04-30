86d7021f90a0b3b500d8e9451a9665d61bdce95b
Johannes Blaschke <jpblaschke@lbl.gov>
2021-04-30 11:45:36 -0700

    done de-duplicating

--- analysis/db.py
+++ analysis/db.py
@@ -41,6 +41,11 @@ class DebugDB(object):
 
 
     @property
+    def timers(self):
+        return tuple(finish for _, _, _, finish, _ in self._data)
+
+
+    @property
     def good_timers(self):
         return tuple(finish for good, _, _, finish, _ in self._data if good)
 
@@ -51,6 +56,11 @@ class DebugDB(object):
 
 
     @property
+    def ranks(self):
+        return tuple(rank for _, rank, _, _, _ in self._data)
+
+
+    @property
     def good_ranks(self):
         return tuple(rank for good, rank, _, _, _ in self._data if good)
 
@@ -58,6 +68,11 @@ class DebugDB(object):
     @property
     def fail_ranks(self):
         return tuple(rank for good, rank, _, _, _ in self._data if not good)
+
+
+    @property
+    def eqs(self):
+        return tuple(tuple([s, *ss, f]) for _, _, s, f, ss in self._data)
 
 
     @property
@@ -76,21 +91,10 @@ class EventQueueDB(DebugDB):
     def __init__(self, ds):
         super().__init__(ds)
 
-        self.__good_eqs       = list()
-        self.__good_starts    = list()
-        self.__good_finishes  = list()
-        for eq, rank in zip(super().good_eqs, super().good_ranks):
-            self.__good_eqs.append(EventQueue(eq, True, rank))
-            self.__good_starts.append(eq[0])
-            self.__good_finishes.append(eq[-1])
-
-        self.__fail_eqs       = list()
-        self.__fail_starts    = list()
-        self.__fail_finishes  = list()
-        for eq, rank in zip(super().fail_eqs, super().fail_ranks):
-            self.__fail_eqs.append(EventQueue(eq, False, rank))
-            self.__fail_starts.append(eq[0])
-            self.__fail_finishes.append(eq[-1])
+        self._eqs = tuple(
+                EventQueue(tuple([start, *steps, finish]), good, rank)
+                for good, rank, start, finish, steps in self._data
+            )
 
         self._end = -1  # TODO: use a max value instead
 
@@ -103,37 +107,50 @@ class EventQueueDB(DebugDB):
     @end.setter
     def end(self, value):
         self._end = value
-        for eq in self.__good_eqs:
+        for eq in self._eqs:
             eq.end = value
-        for eq in self.__fail_eqs:
-            eq.end = value
+
+
+    @property
+    def starts(self):
+        return tuple(eq.start for eq in self._eqs)
 
 
     @property
     def good_starts(self):
-        return self.__good_starts
+        return tuple(eq.start for eq in self._eqs if eq.good)
+
+
+    @property
+    def finishes(self):
+        return tuple(eq.end for eq in self._eqs)
 
 
     @property
     def good_finishes(self):
-        return self.__good_starts
+        return tuple(eq.end for eq in self._eqs if eq.good)
 
 
     @property
     def fail_starts(self):
-        return self.__fail_starts
+        return tuple(eq.start for eq in self._eqs if not eq.good)
 
 
     @property
     def fail_finishes(self):
-        return self.__fail_starts
+        return tuple(eq.end for eq in self._eqs if not eq.good)
+
+
+    @property
+    def eqs(self):
+        return tuple(eq for eq in self._eqs)
 
 
     @property
     def good_eqs(self):
-        return self.__good_eqs
+        return tuple(eq for eq in self._eqs if eq.good)
 
 
     @property
     def fail_eqs(self):
-        return self.__fail_eqs
+        return tuple(eq for eq in self._eqs if not eq.good)
